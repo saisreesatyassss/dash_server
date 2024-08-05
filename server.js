3633ae1b52969b153a2d8daba0400010a148ce6d
saisreesatyassss <saisreesatyassss@gmail.com>
2024-08-06 02:38:38 +0530

    mon

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.use(bodyParser.json());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
+  .then(() => console.log('Mongo DB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
 // Routes
